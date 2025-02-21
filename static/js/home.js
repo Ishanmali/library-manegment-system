01096fcef8c321002a1d35e4f43380a94f0c8f22
Ishan malinda <153257473+Ishanmali@users.noreply.github.com>
2025-02-21 14:33:33 +0530

    preloder

--- static/js/home.js
+++ static/js/home.js
@@ -22,3 +22,9 @@ document.addEventListener('DOMContentLoaded', function() {
 
     changeBackgroundImage(); 
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+    setTimeout(function() {
+        document.querySelector("body").classList.add("loaded");
+    }, 1000)
+  });
