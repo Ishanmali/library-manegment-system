c3e0bab4a7834169f27cdf0097e9ef8bd8f3022e
Ishan malinda <153257473+Ishanmali@users.noreply.github.com>
2025-02-23 00:03:02 +0530

    updete and api

--- static/js/home.js
+++ static/js/home.js
@@ -27,6 +27,46 @@ document.addEventListener("DOMContentLoaded", function() {
     }, 1000)
   });
 
+  document.addEventListener('DOMContentLoaded', function () {
+    const arrowLeft = document.querySelectorAll('.arrow-btn .ri-arrow-left-wide-line');
+    const arrowRight = document.querySelectorAll('.arrow-btn .ri-arrow-right-wide-line');
+
+    arrowLeft.forEach(arrow => {
+        arrow.addEventListener('click', () => {
+            const bookGrid = arrow.closest('.book-section').querySelector('.book-grid');
+            bookGrid.scrollBy({ left: -300, behavior: 'smooth' });
+        });
+    });
+
+    arrowRight.forEach(arrow => {
+        arrow.addEventListener('click', () => {
+            const bookGrid = arrow.closest('.book-section').querySelector('.book-grid');
+            bookGrid.scrollBy({ left: 300, behavior: 'smooth' });
+        });
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    const myapi=   "AIzaSyBXAzmRYtYaq2bBgdMKS3-aUMKt-6uwwJg"     ;
+    const new_book= document.getElementById("new-books");
+    const fiction_books= document.getElementById("fiction-books");
+
+    fetch(`https://www.googleapis.com/books/v1/volumes?q=new&maxResults=8&key=${myapi}`)
+       .then(response => response.json())
+       .then(data =>{
+        const book =data.item;
+        book.forEach(book =>{
+            const book_item= Books(book);
+            new_book.appendChild(book_item);
+        })
+       })
+
+
+       
+    fetch('https://www.googleapis.com/books/v1/volumes?q=new&maxResults=8&key=${myapi}')   
+      
+})
+
 
 
   
